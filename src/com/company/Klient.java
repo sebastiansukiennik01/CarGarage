ad938dfd28ce764f7bdc0f48401fe39294bfc3bb
Sebastian Sukiennik <sebastiansukiennik01@gmail.com>
2022-04-09 17:47:27 +0200

    Added first test class. Class Klient contains 3 fields: nrTelefonu, imie and nazwisko

---
+++ src/com/company/Klient.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Klient {
+
+    String nrTelefonu;
+    String imie;
+    String nazwisko;
+
+    public Klient(String nrTelefonu, String imie, String nazwisko) {
+        this.nrTelefonu = nrTelefonu;
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+    }
+
+}
