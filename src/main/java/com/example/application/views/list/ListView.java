e442973e41d5f17e206723a2a17c5b91f161d28b
Sebastian Sukiennik <sebastiansukiennik01@gmail.com>
2022-04-14 16:01:32 +0200

    Added classes: Klient, Faktura, Samochod, Produkt, Usluga. Added views: KlientView, FakturaView, SamochodView, ProduktView, UslugaView.

--- src/main/java/com/example/application/views/list/ListView.java
+++ src/main/java/com/example/application/views/list/ListView.java
@@ -1,30 +1,35 @@
 package com.example.application.views.list;
 
+import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.board.Board;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.charts.model.Dial;
+import com.vaadin.flow.component.dialog.Dialog;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+
+import java.awt.*;
 
 @PageTitle("list")
 @Route(value = "")
 public class ListView extends VerticalLayout {
 
     public ListView() {
-        add(new H1("Hello world"));
-        add(new H2("Testowy nagłówek"));
+        add(new H1("Car garage"));
+        add(new Text("Tu sobie porobimy zakładki klasy i będzie gituwa\n  Dokumentacja z wszystkimi componentami: https://vaadin.com/docs/latest/ds/components"));
 
-        add(new Button("przycisk"));
-        // dodawanie nowego przycisku bedzei tutaj
+        Dialog dialog = new Dialog();
+        dialog.add("Co za wariat");
 
-
-
-        System.out.println("hello world");
-        System.out.println("hello world 222");
-
+        Button button = new Button("Nie klikniesz", e -> dialog.open());
+        add(dialog, button);
     }
+
+
 }
