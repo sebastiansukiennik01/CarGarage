612000dbcb784fa347da4bdf7d73e1494d8aff0a
Sebastian Sukiennik <sebastiansukiennik01@gmail.com>
2022-05-04 23:51:26 +0200

    dependencies

--- src/main/java/com/example/application/Application.java
+++ src/main/java/com/example/application/Application.java
@@ -4,11 +4,24 @@ import com.vaadin.flow.component.dependency.NpmPackage;
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.theme.Theme;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.EventListener;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
 
 /**
  * The entry point of the Spring Boot application.
@@ -25,6 +38,22 @@ public class Application extends SpringBootServletInitializer implements AppShel
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+}
+
+@Component
+@RequiredArgsConstructor
+class SqlServerDEmo {
+
+    private final JdbcTemplate template;
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void ready() {
+
+        List<Klient> klienci = this.template.query("select * from Clients",
+                (resultSet, i) -> new Klient(resultSet.getString(0), resultSet.getString(1), resultSet.getString(2)));
+        klienci.forEach(System.out::println);
     }
 
 }
